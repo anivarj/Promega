69a9c2d96f2c9075b70ebe58f1eac27969c3b509
Ani Michaud (Varjabedian) <32859488+anivarj@users.noreply.github.com>
2022-09-12 10:20:37 -0500

    updates to script

--- LV200-importer-2.py
+++ LV200-importer-2.py
@@ -7,10 +7,7 @@ Dependencies:
 This script assumes data organization is as follows:
     - A parent folder with all of your data for the experiment
     - Sub folders for each sample (ex: "HEK293_Live_G600_exp1000ms_A1")
-    - Inside each subfolder, a series of .tif files corresponding to avgs to be maximum projected together, ending in "_T001.tif", "_T002.tif", etc.
-
-You can have brightfield images or other things inside the subfolders too. The script will look for the "T00?.tif" files and only pull those. 
-
+    - Inside each subfolder, a series of .tif files corresponding to avgs to be maximum projected together, named the same as the subfolder and ending in "_T001.tif", "_T002.tif", etc.
 
 '''
 
@@ -42,6 +39,8 @@ def make_stacks(subfolder, baseName):
 
     
 ##################################
+############ MAIN  ###############
+##################################
 
 targetWorkspace = askdirectory(title="SELECT YOUR DATA LOCATION") # GUI to choose your raw data location
 outputPath = os.path.join(targetWorkspace, "processed") #sets path to output location
@@ -49,7 +48,7 @@ outputPath = os.path.join(targetWorkspace, "processed") #sets path to output loc
 if os.path.exists(outputPath): #if a processed folder exists already, remove the old one and overwrite.
     print("Processed folder already exists. Overwriting...")
     shutil.rmtree(outputPath)
-os.mkdir(outputPath)
+os.mkdir(outputPath) #re-make the directory
 
 subfolderList = list_subfolders(targetWorkspace) #List the contents of the parent folder and create a list of all the subfolders
 
@@ -58,11 +57,8 @@ for subfolder in subfolderList: #for each subfolder, find all the .tif files and
     print("Starting", baseName)
     stack = make_stacks(subfolder, baseName) #open the .tif files and make a stack
     
-    #MAXdata = np.max(stack, axis=0) #make a maximum projection of the stack
     AVGdata = np.round(np.mean(stack, axis=0)).astype('uint16') #makes average projection. 
     savePath = os.path.join(outputPath, baseName) #Sets the path for saving the stack. Inside "processed" under the same name as the original subfolder
-    #tifffile.imwrite(savePath+"_max.tif", MAXdata) #saves the max projections with "_max" suffix
     tifffile.imwrite(savePath+"_avg.tif", AVGdata) #saves average projection with "_avg" suffix
-    #tifffile.imwrite(savePath+".tif", stack) #option for saving the unprojected stack
 
 print("Done with script!")
